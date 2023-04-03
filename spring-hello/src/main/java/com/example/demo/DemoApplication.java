c31b76240d5b5027b448cfb876e3b9eaa8b68caa
maeddes <maeddes@gmail.com>
2023-04-03 09:30:16 +0000

    Spring Initial Lecture

---
+++ spring-hello/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,29 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+
+	@GetMapping("/hello")
+	public String sayHello(){
+
+		return "Hallo Esslingen Summer Semester 23";
+
+	}
+
+	@PostMapping("/endpoint/{variable}")
+	public String postVariable(@PathVariable String variable){
+
+		System.out.println(" #### Submitted variable: "+variable);
+		return "The variable which has been submitted is: "+variable;
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
